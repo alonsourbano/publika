501595ef5272f9b3443d50eb65132cc151be04c6
Alonso Urbano <vladimir.urbano@finnair.com>
2022-10-20 14:10:03 +0300

    Cluster

--- publika.js
+++ publika.js
@@ -7,7 +7,7 @@ Module.register("publika", {
     fontawesomeCode: undefined,
 
     initialLoadDelay: 0 * 1000, // N seconds delay
-    updateInterval: 50 * 1000, // every N seconds
+    updateInterval: 20 * 1000, // every N seconds
     retryDelay: 50 * 1000, // every N seconds
 
     apiURL: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
@@ -142,18 +142,43 @@ Module.register("publika", {
     var headerRow = `<tr class="stop-header"><th ${this.colspan
       }>${this.getHeaderRow(stop)}</th></tr><tr class="stop-subheader"><td ${this.colspan
       }>${this.getSubheaderRow(stop)}<td></tr>`;
-    var rows = stop.stopTimes
+    var rows = this.getStopTimes(stop.stopTimes)
       .map((item) => `<tr>${this.getRowForTimetable(item)}</tr>`)
       .reduce((p, c) => `${p}${c}`, "");
+    const stopAlerts = this.getAlerts(stop.alerts);
     var alerts =
-      stop.alerts.length > 0
-        ? stop.alerts.map(
+      stopAlerts.length > 0
+        ? stopAlerts.map(
           (alert) =>
             `<tr><td ${this.colspan}>${this.getAlertIcon()} ${alert.alertHash
             }<td></tr>`
         )
         : "";
     return `${headerRow}${rows}${alerts}`;
+  },
+
+  getStopTimes: function (stopTimes) {
+    if (stopTimes.length === 0) {
+      return stopTimes;
+    }
+    if (!Array.isArray(stopTimes.at(0))) {
+      return stopTimes;
+    }
+    return stopTimes
+      .reduce((p, c) => [...p, ...c], [])
+      .sort((a, b) => a.ts - b.ts);
+  },
+
+  getAlerts: function (alerts) {
+    if (alerts.length === 0) {
+      return alerts;
+    }
+    if (!Array.isArray(alerts.at(0))) {
+      return alerts;
+    }
+    return alerts
+      .reduce((p, c) => [...p, ...c], [])
+      .sort((a, b) => a.effectiveStartDate - b.effectiveStartDate);
   },
 
   getRowForTimetable: function (item) {
@@ -225,15 +250,17 @@ Module.register("publika", {
 
   getSubheaderRow: function (stop) {
     const items =
-      stop.locationType === "STATION"
+      stop.locationType === "STOP"
         ? [
-          `<span class="stop-code">${this.translate("STATION")}</span>`,
-          `<span class="stop-zone">${stop.zoneId}</span>`
-        ]
-        : [
           stop.desc,
           `<span class="stop-code">${stop.code}</span>`,
           `<span class="stop-zone">${stop.zoneId}</span>`
+        ]
+        : [
+          `<span class="stop-code">${this.translate(
+            stop.locationType
+          )}</span>`,
+          this.getZoneId(stop.zoneId)
         ];
     if (stop.platformCode) {
       items.splice(
@@ -252,16 +279,30 @@ Module.register("publika", {
     return items.reduce((p, c) => `${p} ${c}`, "");
   },
 
-  getStopNameWithVehicleMode: function (item, includeId = undefined) {
-    const name = includeId ? `${includeId} • ${item.name}` : item.name;
-    return this.config.fontawesomeCode
-      ? `<i class="${this.getVehicleModeIcon(item.vehicleMode)}"></i> ${name}`
-      : `${name} (${this.translate(item.vehicleMode)})`;
+  getZoneId: function (zones) {
+    if (!Array.isArray(zones)) {
+      zones = [zones];
+    }
+    zones = [...new Set(zones)];
+    return zones
+      .map((zone) => `<span class="stop-zone">${zone}</span>`)
+      .reduce((p, c) => `${p}${c}`, "");
   },
 
-  getVehicleModeIcon: function (vehicleMode) {
+  getStopNameWithVehicleMode: function (item, includeId = undefined) {
+    const name = includeId ? `${includeId} • ${item.name}` : item.name;
+    if (!Array.isArray(item.vehicleMode)) {
+      item.vehicleMode = [item.vehicleMode];
+    }
+    item.vehicleMode = [...new Set(item.vehicleMode)];
+    return this.config.fontawesomeCode
+      ? `${this.getVehicleModeIcon(item.vehicleMode)} ${name}`
+      : `${name} (${this.getVehicleModeText(item.vehicleMode)})`;
+  },
+
+  getVehicleModeIcon: function (vehicleModes) {
     // Vehicle modes according to HSL documentation
-    return new Map([
+    const map = new Map([
       ["AIRPLANE", "fa-solid fa-plane-up"],
       ["BICYCLE", "fa-solid fa-bicycle"],
       ["BUS", "fa-solid fa-bus-simple"],
@@ -273,7 +314,16 @@ Module.register("publika", {
       ["RAIL", "fa-solid fa-train"],
       ["SUBWAY", "fa-solid fa-m"],
       ["TRAM", "fa-solid fa-train-tram"]
-    ]).get(vehicleMode);
+    ]);
+    return vehicleModes
+      .map((mode) => `<i class="${map.get(mode)}"></i>`)
+      .reduce((p, c) => `${p}${c}`, "");
+  },
+
+  getVehicleModeText: function (vehicleModes) {
+    return vehicleModes
+      .map((mode) => this.translate(mode))
+      .reduce((p, c) => `${p}, ${c}`, "");
   },
 
   getAlertIcon: function () {
@@ -282,9 +332,9 @@ Module.register("publika", {
       : "!!!";
   },
 
-  getPlatformText: function (vehicleMode) {
+  getPlatformText: function (vehicleModes) {
     const defaultText = this.translate("PLATFORM");
-    return new Map([
+    const map = new Map([
       ["AIRPLANE", defaultText],
       ["BICYCLE", defaultText],
       ["BUS", defaultText],
@@ -296,6 +346,9 @@ Module.register("publika", {
       ["RAIL", this.translate("TRACK")],
       ["SUBWAY", this.translate("TRACK")],
       ["TRAM", defaultText]
-    ]).get(vehicleMode);
+    ]);
+    return vehicleModes
+      .map((mode) => map.get(mode))
+      .reduce((p, c) => `${p}${c}`, "");
   }
 });
